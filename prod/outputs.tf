f8d357bb52d9a94bc390d87ac22bd2adeef485d1
shazil711 <shazi85@gmail.com>
2026-09-18 14:50:53 +0530

    Add prod environment

--- prod/outputs.tf
+++ prod/outputs.tf
@@ -1,2 +1,2 @@
 output "vm_public_ip"  { value = module.compute.public_ip }
-output "network_name"  { value = module.network.network_name }
+output "network_name"  { value = module.networking.network_name }
